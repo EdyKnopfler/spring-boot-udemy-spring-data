a6a6e4942267c3935363c267bb6078f715e1fcf4
Éderson Cássio <edersoncassiolf@outlook.com>
2023-06-06 22:21:52 -0300

    Busca por nome

--- src/main/java/com/derso/vendas/controller/ClientesController.java
+++ src/main/java/com/derso/vendas/controller/ClientesController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.derso.vendas.domain.Cliente;
@@ -29,8 +30,10 @@ public class ClientesController {
 	private ClientesRepository repositorio;
 	
 	@GetMapping
-	public List<Cliente> todos() {
-		return repositorio.findAll();
+	public List<Cliente> listar(@RequestParam(name="nome", required=false) String nomeBusca) {
+		return nomeBusca != null
+				? repositorio.findByNomeLikeIgnoreCase("%" + nomeBusca + "%")
+				: repositorio.findAll();
 	}
 
 	@PostMapping
--- src/main/java/com/derso/vendas/repository/ClientesRepository.java
+++ src/main/java/com/derso/vendas/repository/ClientesRepository.java
@@ -1,11 +1,13 @@
 package com.derso.vendas.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.derso.vendas.domain.Cliente;
 
 public interface ClientesRepository extends JpaRepository<Cliente, Long> {
 	
-	
+	List<Cliente> findByNomeLikeIgnoreCase(String nome);
 
 }
