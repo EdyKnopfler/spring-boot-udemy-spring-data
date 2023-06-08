e6d7b4c6ceb2d53cabea9096efbe183beb8c15be
Éderson Cássio <edersoncassiolf@outlook.com>
2023-06-08 18:57:03 -0300

    Usando ResponseEntity para tratar a resposta

--- src/main/java/com/derso/vendas/controller/ClientesController.java
+++ src/main/java/com/derso/vendas/controller/ClientesController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,9 +39,22 @@ public class ClientesController {
 				: repositorio.findAll();
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> porId(@PathVariable("id") long id) {
+		Optional<Cliente> cliente = repositorio.findById(id);
+		 
+		return cliente.isPresent()
+				? ResponseEntity.ok(cliente.get())
+				: ResponseEntity.notFound().build();
+	}
+	
 	@GetMapping("/cpf/{cpf}")
-	public Cliente porCpf(@PathVariable("cpf") String cpf) {
-		return repositorio.findOneByCpf(cpf);
+	public ResponseEntity<Cliente> porCpf(@PathVariable("cpf") String cpf) {
+		Cliente cliente = repositorio.findOneByCpf(cpf);
+		
+		return cliente != null
+				? ResponseEntity.ok(cliente)
+				: ResponseEntity.notFound().build();
 	}
 
 	@PostMapping
