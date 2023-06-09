58ce46cce9d8d54749223f2d506adbff41df98ee
Éderson Cássio <edersoncassiolf@outlook.com>
2023-06-09 02:51:02 -0300

    Não estamos usando

--- src/main/java/com/derso/vendas/dto/PedidoDTO.java
+++ src/main/java/com/derso/vendas/dto/PedidoDTO.java
@@ -4,6 +4,5 @@ import java.util.List;
 
 public record PedidoDTO(
 	long clienteId,
-	double totalEsperado,  // Esperamos receber o total do front-end para verificar discrepância
 	List<ItemPedidoDTO> itens) {
 }
