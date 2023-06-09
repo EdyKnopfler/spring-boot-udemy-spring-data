764e3d6578168be1fd371cef61f7ef9fbe7bad11
Éderson Cássio <edersoncassiolf@outlook.com>
2023-06-09 01:57:38 -0300

    Faltou a anotação para ativar os @Transactional

--- src/main/java/com/derso/vendas/VendasApplication.java
+++ src/main/java/com/derso/vendas/VendasApplication.java
@@ -2,8 +2,10 @@ package com.derso.vendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class VendasApplication {
 
 	public static void main(String[] args) {
