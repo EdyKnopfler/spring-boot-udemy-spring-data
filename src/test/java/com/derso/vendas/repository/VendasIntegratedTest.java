afaa6092b0ead93a40c1ee2d355d88411b58c07a
Éderson Cássio <edersoncassiolf@outlook.com>
2023-06-09 03:03:32 -0300

    Tinha uma import aqui esperando para ser removido

--- src/test/java/com/derso/vendas/repository/VendasIntegratedTest.java
+++ src/test/java/com/derso/vendas/repository/VendasIntegratedTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.derso.vendas.domain.ItemPedido;
 import com.derso.vendas.domain.Pedido;
